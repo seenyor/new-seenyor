4adcccc3d4b41672f9982aa4f7121ef430b09386
Tafhim87 <tafhimhasan482@gmail.com>
2025-03-10 23:46:38 +0600

    next button

--- src/components/SystemBuild/DeviceInfo.jsx
+++ src/components/SystemBuild/DeviceInfo.jsx
@@ -134,7 +134,7 @@ const DeviceInfo = () => {
           {verified && devices.length > 0 && (
             <Link
               href="/register"
-              className="w-[800px] active:scale-95 text-center bg-gradient-to-r from-[#A2CDB9] to-[#5BAE87] text-white text-[18px] px-8 py-3 rounded-[15px]"
+              className="w-[800px] md:w-full active:scale-95 text-center bg-gradient-to-r from-[#A2CDB9] to-[#5BAE87] text-white text-[18px] px-8 py-3 rounded-[15px]"
             >
               Next
             </Link>
