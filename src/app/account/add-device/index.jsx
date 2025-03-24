5bc623a4dc8f78e31969e23f5d73696c828b0866
Majharul Islam <majharul@codevaly.com>
2025-03-24 20:46:32 +0600

    Adding device to existing user

--- src/app/account/add-device/index.jsx
+++ src/app/account/add-device/index.jsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "@/components/SystemBuild/Loader";
 import Link from "next/link";
 import DeviceCard from "@/components/SystemBuild/DeviceCard";
 import AddLoader from "./AddLodder";
 import AddDeviceCard from "./AddDeviceCard";
-
+import { useUserService } from "@/services/userService";
+import { toast } from "react-toastify";
 const AddDevice = () => {
   const [deviceUid, setDeviceUid] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
@@ -14,26 +15,41 @@ const AddDevice = () => {
   const [verified, setVerified] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const [email, setEmail] = useState("");
+  const [devices, setDevices] = useState([]);
+  const [user, serUser] = useState([]);
+
   const [emailValid, setEmailValid] = useState(true);
   const [isEmpty, setIsEmpty] = useState(false);
-
+  const { getUserDetailsById, chnageDeviceStatus } = useUserService();
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!deviceUid.trim()) {
       setErrorMessage("Please enter your Device UID.");
       return;
     }
-
     setErrorMessage("");
     setLoading(true);
-
     setTimeout(() => {
       setLoading(false);
       setVerified(true);
     }, 2000);
   };
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("user_id");
+    if (storedUserId) {
+      fetchUserDetails(storedUserId);
+    }
+  }, []);
 
+  const fetchUserDetails = async (id) => {
+    try {
+      const userDetails = await getUserDetailsById(id);
+      setDevices(userDetails?.data?.uids);
+      serUser(userDetails?.data);
+    } catch (error) {
+      console.error("Failed to fetch user details:", error);
+    }
+  };
   const handleEmailChange = (e) => {
     const value = e.target.value;
     setEmail(value);
@@ -41,10 +57,46 @@ const AddDevice = () => {
     setEmailValid(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value));
   };
 
-  const handleContinue = () => {
-    if (!isEmpty && emailValid) {
-      setShowPopup(false);
-      console.log("Email submitted:", email);
+  const handleContinue = async (e) => {
+    e.preventDefault();
+    if (deviceUid.length !== 12) {
+      toast.error("Device UID must be exactly 12 characters long.");
+      return;
+    }
+
+    const storedDevices = JSON.parse(localStorage.getItem("devices")) || [];
+
+    if (devices.includes(deviceUid) || storedDevices.includes(deviceUid)) {
+      toast.error("This Device UID has already been added.");
+      return;
+    }
+
+    // setLoading(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/deals/device-details?uid=${deviceUid}`
+      );
+      const data = await response.json();
+      console.log("API Response:", data);
+
+      if (data.is_active === true || data.message === "Device not found") {
+        toast.error("Device UID is not valid.");
+        // setLoading(false);
+        return;
+      } else {
+        const chnageStatus = chnageDeviceStatus({
+          uids: [deviceUid],
+          email: user?.email,
+          is_active: true,
+        });
+        toast.success("Device Added Successfully!");
+        setShowPopup(false);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error fetching device details:", error);
+      toast.error("An error occurred while verifying the device.");
     }
   };
 
@@ -54,7 +106,7 @@ const AddDevice = () => {
 
       {!loading && (
         <div className="container max-w-[650px] font-poppins text-[#1D293F] items-start overflow-x-hidden justify-center flex flex-col gap-[60px] mt-[-50px] md:mt-0">
-          <AddDeviceCard />
+          <AddDeviceCard uids={devices} />
           <Link
             href="#"
             onClick={(e) => {
@@ -87,10 +139,10 @@ const AddDevice = () => {
                       type="email"
                       placeholder="ex: 4678469JDUE7DBFE8N"
                       className="w-[435px] md:w-full text-[#1D293F] h-[60px] text-[20px] p-3 border border-gray-300 border-solid rounded-xl"
-                      value={email}
-                      onChange={handleEmailChange}
+                      value={deviceUid}
+                      onChange={(e) => setDeviceUid(e.target.value)}
                     />
-                    {isEmpty && (
+                    {/* {isEmpty && (
                       <p className="text-red-500 text-sm mb-4 w-full">
                         Email cannot be empty. Please enter your email.
                       </p>
@@ -99,7 +151,7 @@ const AddDevice = () => {
                       <p className="text-red-500 text-sm mb-4 w-full">
                         Please enter a valid email address.
                       </p>
-                    )}
+                    )} */}
                   </div>
                   <button
                     onClick={handleContinue}
